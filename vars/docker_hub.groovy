6396aa6e04f0fc219ae1be0435c1c73dd22a6cda
Sourabh lodhi <140099778+Sourabh9125@users.noreply.github.com>
2025-07-15 12:55:47 +0530

    Update docker_hub.groovy

--- vars/docker_hub.groovy
+++ vars/docker_hub.groovy
@@ -1,10 +1,13 @@
-def call(String credentialsId, String image, String tag){
+def call(String credentialsId, String image, String tag , Map config [:]){
+     def credentialsId = config.credentialsId ?: error "please provide credentialsId"
+     def imageName = config.imageName ?: error "give the image name"
+     def imageTag = config.imageTag ?: "latest"
      withCredentials([usernamePassword(
                 credentialsId:"${credentialsId}",
                 usernameVariable: "dockerHubUser",
                 passwordVariable: "dockerHubPass")]){
             sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
-            sh "docker image tag ${image} ${env.dockerHubUser}/${image}:${tag}"
-            sh "docker push ${env.dockerHubUser}/${image}:${tag}"
+            sh "docker image tag ${imageName} ${env.dockerHubUser}/${imageName}:${imageTag}"
+            sh "docker push ${env.dockerHubUser}/${imageName}:${imageTag}"
         }
 }
