73d03988702c62d49c2db23d1e3ddff4e9ab24d0
Sourabh lodhi <140099778+Sourabh9125@users.noreply.github.com>
2025-07-15 12:51:48 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,12 @@
-def call(String image, String tag){
-    sh "docker build -t ${image}:${tag} . "
+def call(Map config= [:]){
+    def imageName = config.imageName ?: error ("Image name is required")
+    def imageTag = config.imageTag ?: 'latest'
+    def context = config.context ?: '.'
+    def dockerfile = config.dockerfile ?: 'Dockerfile'
+
+    echo "docker build -t ${imageName}:${imageTag} ${dockerfile} "
+
+    sh """
+       docker build -t ${imageName}:${imageTag} -f ${dockerfile} ${context}
+       """
 }
